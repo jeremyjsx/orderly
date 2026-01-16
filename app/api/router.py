67400cfb5de2bae9f07ed61c3eb63384e15fd1c8
JeremyDevCode <jeremydevcode@gmail.com>
2026-01-16 12:20:16 -0500

    feat(products): add product endpoints

--- app/api/router.py
+++ app/api/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from app.modules.auth.router import router as auth_router
+from app.modules.products.router import router as products_router
 
 router = APIRouter()
 
 router.include_router(auth_router)
+router.include_router(products_router)
